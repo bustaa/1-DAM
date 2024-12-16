375511c39bd83e22ddba78032520e60925145744
ALUMNADO\mibucu <mibucu@alumnatflorida.es>
2024-12-16 19:59:33 +0100

    AP6 finalizada

--- PRO/AP6/Ejercicio01/Program.cs
+++ PRO/AP6/Ejercicio01/Program.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 
-namespace Ejercicio01
+namespace Ejercicio01 
 {
-     class Program
+    class Program
     {
-        static void Main()
+        static void Main(string[] args) 
         {
             Console.WriteLine(" ** PROMEDIO DE NOTAS ** ");
             Console.WriteLine(" ");
 
             string alumno;
-            int nota1, nota2, nota3;
+            float nota1, nota2, nota3;
 
             Console.Write("Ingrese nombre del alumno: ");
             alumno = Console.ReadLine();
             Console.Write("Ingrese la nota 1: ");
-            nota1 = int.Parse(Console.ReadLine());
+            nota1 = float.Parse(Console.ReadLine());
             Console.Write("Ingrese la nota 2: ");
-            nota2 = int.Parse(Console.ReadLine());
+            nota2 = float.Parse(Console.ReadLine());
             Console.Write("Ingrese la nota 3: ");
-            nota3 = int.Parse(Console.ReadLine());
+            nota3 = float.Parse(Console.ReadLine());
 
-            double promedio = (nota1 + nota2 + nota3) / 3.0;
+            double promedio = (nota1 + nota2 + nota3) / 3.00;
 
-            Console.WriteLine("------------------------------------");
+            Console.WriteLine("---------------------------------");
             Console.WriteLine("EL PROMEDIO: " + promedio.ToString("0.00"));
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
---
+++ PRO/AP6/Ejercicio04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ** CONTROL DE VENTA DE PRODUCTOS ** ");
+            Console.WriteLine(" ");
+
+            double precio;
+            int cantidad;
+
+            Console.Write("Ingrese precio del producto: $ ");
+            precio = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese cantidad comprada: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+            double importe = cantidad * precio;
+            double descuento = 11 / 100.0 * importe;
+            double total = importe - descuento;
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("IMPORTE TOTAL: $ " + importe.ToString("0.00"));
+            Console.WriteLine("DESCUENTO    : $ " + descuento.ToString("0.00"));
+            Console.WriteLine("TOTAL A PAGAR: $ " + total.ToString("0.00"));
+
+            Console.ReadKey();
+        }
+    }
+}
