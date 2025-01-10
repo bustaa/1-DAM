267970658e74ea20302068830d545dee4a127243
ALUMNADO\mibucu <mibucu@alumnatflorida.es>
2025-01-10 17:23:33 +0100

    Inicio ej16

---
+++ PRO/AP7/Ejercicio16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace Ejercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList temperatura = new ArrayList();
+            float temp;
+            float tempMax = float.MinValue, tempMin = float.MaxValue;
+            float promedio = 0;
+
+            do
+            {
+                Console.Write("Dime una temperatura: ");
+                temp = float.Parse(Console.ReadLine());
+                if (temp != 99)
+                {
+                    temperatura.Add(temp);
+                    promedio += temp;
+                }
+
+            } while (temp != 99);
+        }
+    }
+}
