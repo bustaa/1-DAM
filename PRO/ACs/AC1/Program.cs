3746a1ae0d6670a72f2993bac0c093efd1a00f0e
ALUMNADO\mibucu <mibucu@alumnatflorida.es>
2025-02-04 18:55:19 +0100

    AC1 realizada

---
+++ PRO/ACs/AC1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raices ecu1 = new Raices(3, 4, 1);
+            ecu1.Calcular();
+
+            Raices ecu2 = new Raices(1, 4, 4);
+            ecu2.Calcular();
+
+            Raices ecu3 = new Raices(8, 2, 5);
+            ecu3.Calcular();
+        }
+    }
+}
---
+++ PRO/ACs/AC1/Raices.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace AC1
+{
+    class Raices
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double sol1;
+        private double sol2;
+
+        public Raices(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        private double getDiscriminante()
+        {
+            return Math.Pow(b, 2) - 4 * a * c;
+        }
+
+        private bool TieneRaices()
+        {
+            if (getDiscriminante() > 0) return true;
+            return false;
+        }
+
+        private bool TieneRaiz()
+        {
+            if (getDiscriminante() == 0) return true;
+            return false;
+        }
+
+        private void ObtenerRaices()
+        {
+            Console.WriteLine($"\n\tLa solución 1 es: {sol1}\n\tLa solución 2 es: {sol2}");
+        }
+
+        private void ObtenerRaiz()
+        {
+            Console.WriteLine($"\n\tLa solución es: {sol1}");
+        }
+
+        public void Calcular()
+        {
+            if (TieneRaices())
+            {
+                sol1 = (-b + Math.Sqrt(getDiscriminante())) / (2 * a);
+                sol2 = (-b - Math.Sqrt(getDiscriminante())) / (2 * a);
+
+                ObtenerRaices();
+            }
+            else if (TieneRaiz())
+            {
+                sol1 = -b / (2 * a);
+
+                ObtenerRaiz();
+            }
+            else
+            {
+                Console.WriteLine("\n\tLa ecuación no tiene una solución posible en R\n");
+            }
+        }
+    }
+}
