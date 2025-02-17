e39c5109ba070b1c1691454ff73071b000a9120d
Miguel <mabusta.06@gmail.com>
2025-02-17 17:16:45 +0100

    Vehiculo.cs

---
+++ PRO/ACs/Vehiculo/Vehiculo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vehiculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo miCoche = new Vehiculo("Toyota", "Corolla", 2020);
+            miCoche.MostrarInfo();
+            miCoche.Acelerar(30);
+            miCoche.MostrarInfo();
+            miCoche.Frenar(10);
+            miCoche.MostrarInfo();
+        }
+    }
+}
---
+++ PRO/ACs/Vehiculo/Vehiculo/Vehiculo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Vehiculo
+{
+    class Vehiculo
+    {
+        private string marca;
+        private string modelo;
+        private int anio;
+        private int velocidadActual = 0;
+
+        /// <summary>
+        /// Constructor de la clase vehiculo que toma los valores directos de Program
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="modelo"></param>
+        /// <param name="anio"></param>
+        public Vehiculo (string marca, string modelo, int anio) 
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.anio = anio;
+        }
+
+        /// <summary>
+        /// Aumenta la velocidad del vehiculo
+        /// </summary>
+        /// <param name="cantidad">km/h incrementados para la velocidad</param>
+        public void Acelerar(int cantidad) 
+        {
+            velocidadActual += cantidad;
+        }
+
+        public void Frenar(int cantidad) 
+        {
+            velocidadActual -= cantidad;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"{marca} {modelo} ({anio} - Velocidad: {velocidadActual} km/h)");
+        }
+    }
+}
